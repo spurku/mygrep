8ce37b0cb522088356f292c57501ff8e698d993e
spurku <janne.lankinen1@gmail.com>
2025-03-04 23:54:57 +0200

    added makefile

--- inkrement_2/mygrep.cpp
+++ inkrement_2/mygrep.cpp
@@ -1,30 +1,37 @@
-# include <iostream>
-# include <string>
-# include <fstream>
+#include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
 
-int grep(string str, string _path) {
+int grep(const string& str, const string& _path) {
     ifstream file(_path);
-    string line;
     if (!file.is_open()) {
+        cerr << "Error: Could not open file " << _path << endl;
         return 1;
     }
 
+    string line;
+    bool found = false;
     while (getline(file, line)) {
         if (line.find(str) != string::npos) {
             cout << line << endl;
+            found = true;
         }
+    }
+
+    if (!found) {
+        cout << "No matches found for \"" << str << "\" in " << _path << endl;
     }
 
     file.close();
     return 0;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        cout << "Usage: " << argv[0] << " <search string> <file path>" << endl;
-        return 0;
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        cerr << "Usage: " << argv[0] << " <search string> <file path>" << endl;
+        return 1;
     }
 
-    grep(argv[1], argv[2]);
+    return grep(argv[1], argv[2]);
 }
